3f5d8a459653d4f0ed1ad27122c27f52868b8843
Sachit71 <72592470+Sachit71@users.noreply.github.com>
2021-03-10 14:02:06 +0530

    Update Password Generator.py
    Awesome code earlier,but I think it was lengthy and impractical so here is a small one,Hope you like it,I came across yours on the net, also please do visit my repositories.

--- Password Generator.py
+++ Password Generator.py
@@ -1,63 +1,16 @@
+import string
 import random
+import pyautogui
 
-def generatePassword(pwlength):
+X=pyautogui.alert("Press enter")
 
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
+def generatePassword(num):
+    password=""
 
-    passwords = [] 
-
-    for i in pwlength:
+    for n in range(num):
+        x=random.randint(0,94)
+        password+=string.printable[x]
         
-        password = "" 
-        for j in range(i):
-            next_letter_index = random.randrange(len(alphabet))
-            password = password + alphabet[next_letter_index]
-        
-        password = replaceWithNumber(password)
-        password = replaceWithUppercaseLetter(password)
-        
-        passwords.append(password) 
-    
-    return passwords
+    return password
 
-
-def replaceWithNumber(pword):
-    for i in range(random.randrange(1,3)):
-        replace_index = random.randrange(len(pword)//2)
-        pword = pword[0:replace_index] + str(random.randrange(10)) + pword[replace_index+1:]
-        return pword
-
-
-def replaceWithUppercaseLetter(pword):
-    for i in range(random.randrange(1,3)):
-        replace_index = random.randrange(len(pword)//2,len(pword))
-        pword = pword[0:replace_index] + pword[replace_index].upper() + pword[replace_index+1:]
-        return pword
-
-
-
-def main():
-    
-    numPasswords = int(input("How many passwords do you want to generate? "))
-    
-    print("Generating " +str(numPasswords)+" passwords")
-    
-    passwordLengths = []
-
-    print("Minimum length of password should be 3")
-
-    for i in range(numPasswords):
-        length = int(input("Enter the length of Password #" + str(i+1) + " "))
-        if length<3:
-            length = 3
-        passwordLengths.append(length)
-    
-    
-    Password = generatePassword(passwordLengths)
-
-    for i in range(numPasswords):
-        print ("Password #"+str(i+1)+" = " + Password[i])
-
-
-
-main()
+pyautogui.alert("Your password is""  "+ generatePassword(20))
